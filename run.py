d694a32a771f6182bea2ad1d1998d9c8ea3ba151
AlexMaitland <alex.k.maitland@gmail.com>
2023-05-09 14:17:38 +0000

    Retrieve latest stock row from worksheet

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -63,6 +63,30 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print('Sales worksheet successfully updated... \n')
 
-data = get_sales_data()
-sales_data = [int(num) for num in data ]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare the sales and stock of each item.
+
+    Results are calculated by subtracting sale figures from the stock.
+    Negative results indicate that extra items had to be made.
+    Positive results mean that there was a surplus.
+    """
+    print('Calculating surplus data....\n')
+
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Function calling all main functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data ]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print('Welcome to Love Sandwiches Data Automation \n')
+main()    
